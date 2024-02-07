433c171aa19260d832aa8ce557e14f7283e4b5e3
Soham Mukherjee <115096630+SohamMukherjeee@users.noreply.github.com>
2024-02-08 01:01:51 +0530

    Add files via upload

---
+++ 10_confusion.js
@@ -0,0 +1,25 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// console.log("2" === 2);
+
+// const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+
+const balance = new Number(100)
+console.log(balance.toFixed(1));
+
+
---
+++ 9_objects.js
@@ -0,0 +1,8 @@
+const course={
+    coursename:"java with soham",
+    price:"999",
+    courseInstructor:"Soham Mukherjee"
+}
+
+const{courseInstructor : master}=course;
+console.log(master);
